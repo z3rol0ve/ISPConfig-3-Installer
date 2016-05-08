d023891939dfc720cb82073183e3289035b88334
z3rol0ve <thanhnam.hoang@gmail.com>
2016-05-08 19:39:01 +0700

    Update ubuntu.functions.sh

--- functions/ubuntu.functions.sh
+++ functions/ubuntu.functions.sh
@@ -417,16 +417,16 @@ sed -i "s/10 03 * * * www-data/#10 03 * * * www-data/" /etc/cron.d/awstats
 
 ubuntu.install_Jailkit (){
 #Install Jailkit
-apt-get -y install build-essential autoconf automake1.9 libtool flex bison debhelper binutils-gold
+apt-get -y install python build-essential autoconf automake1.9 libtool flex bison debhelper binutils-gold
 
 cd /tmp
-wget http://olivier.sessink.nl/jailkit/jailkit-2.17.tar.gz
-tar xvfz jailkit-2.17.tar.gz
-cd jailkit-2.17
+wget http://olivier.sessink.nl/jailkit/jailkit-2.19.tar.gz
+tar xvfz jailkit-2.19.tar.gz
+cd jailkit-2.19
 ./debian/rules binary
 cd ..
-dpkg -i jailkit_2.17-1_*.deb
-rm -rf jailkit-2.17*
+dpkg -i jailkit_2.19-1_*.deb
+rm -rf jailkit-2.19*
 
 } #end function ubuntu.install_Jailkit
 
