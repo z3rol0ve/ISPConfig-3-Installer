18c3fabf89d1bf061535c6ec453bcafe4a54950d
z3rol0ve <thanhnam.hoang@gmail.com>
2016-05-11 09:28:43 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -56,13 +56,6 @@ if [ $DISTRIBUTION == "ubuntu" ] && [ $DISTRIBUTION_VERSION = "xenial" ]; then
 		########Question Procedure
 		apt-get update;
 		apt-get install whiptail;
-		#if SERVER_IP is defined do confirmation
-		if [ $SERVER_IP != "" ]; then
-			if ! (whiptail --title "IP Address Check" --backtitle "$whiptail_title" --yesno "Is the Main IP of the Server? $SERVER_IP" 10 50) then
-				SERVER_IP="";
-			fi;
-		fi;
-    
 		
 	fi;
 else
