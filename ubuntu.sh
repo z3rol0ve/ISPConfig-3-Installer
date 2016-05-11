5a91bff73cb5f8ac041280e3a4af8a31ee549239
z3rol0ve <thanhnam.hoang@gmail.com>
2016-05-11 07:54:09 +0700

    Create ubuntu.sh
    Complete rework

---
+++ ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+####################################################
+# Automate ISPConfig Installation
+# Nam Hoang
+# https://github.com/z3rol0ve/ISPConfig-3-Installer
+####################################################
