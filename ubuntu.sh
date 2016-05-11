2e85683728a0529575b9178bb36d329d412b7b5a
z3rol0ve <thanhnam.hoang@gmail.com>
2016-05-11 09:08:20 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,3 +5,89 @@
 # Nam Hoang
 # https://github.com/z3rol0ve/ISPConfig-3-Installer
 ####################################################
+
+#if user is not root then exit script
+if [ $(id -u) != "0" ]; then
+  echo "Error: You must be root to run this script, please use the root user to install the software.";
+  exit 1;
+fi;
+#if ispconfig is installed exit script
+if [ -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
+  echo "Error: You must be root to run this script, please use the root user to install the software.";
+  exit 1;
+fi;
+
+# Default Distribution
+DISTRIBUTION="";
+# Default Distribution version
+DISTRIBUTION_VERSION="";
+# Default Server Main IP
+SERVER_IP="";
+# Default Hostname short
+HOSTNAME_SHORT="";
+# Default Hostname full / FQDN
+HOSTNAME_FULL="";
+
+###### DETECTING OS & ITS VERSION
+#if lsb_release command exist do
+if command -v lsb_release &> /dev/null; then
+  #detect if lsb_release command return contain  the word ubuntu
+	if lsb_release -a 2> /dev/null | grep -iq "ubuntu"; then
+		# Set Distribution To Ubuntu
+    DISTRIBUTION=ubuntu;
+    DISTRIBUTION_VERSION=$(lsb_release -sc);
+	fi;
+fi;
+###### GETTING SERVER MAIN IP
+#if all needed command exist do
+if [ $(command -v ip) ] && [ $(command -v cut) ] && [ $(command -v head) ] && [ $(command -v tail) ]; then
+  if [ ! -f /proc/user_beancounters ]; then
+    SERVER_IP=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1 | head -2 | tail -1);
+  else
+  	SERVER_IP=$(ip -f inet -o addr show venet0|cut -d\  -f 7 | cut -d/ -f 1 | head -2 | tail -1);
+  fi;
+fi;
+
+whiptail_title="ISPConfig Installer";
+####### MAIN INSTALLATION
+if [ $DISTRIBUTION == "ubuntu" ] && [ $DISTRIBUTION_VERSION = "xenial" ]; then
+  #if apt-get command exist do
+  if command -v apt-get &> /dev/null; then
+    #Question Procedure
+    apt-get update;
+    apt-get install whiptail;
+    #if SERVER_IP is defined do confirmation
+    if [ $SERVER_IP != "" ]; then
+      if ! (whiptail --title "IP Address Check" --backtitle "$whiptail_title" --yesno "Is the Main IP of the Server? $SERVER_IP" 10 50) then
+	      SERVER_IP="";
+      fi;
+    fi;
+    
+    #if SERVER_IP is not defined do loop till user input
+    while [ $SERVER_IP == "" ]; do
+  	  SERVER_IP=$(whiptail --title "Server IP" --backtitle "$whiptail_title" --inputbox "Please specify a Server IP" --nocancel 10 50 3>&1 1>&2 2>&3)
+  	done;
+  	
+  	#if $HOSTNAME_SHORT is not defined do loop till user input
+  	while [ $HOSTNAME_SHORT == "" ]; do
+      HOSTNAME_SHORT=$(whiptail --title "Short Hostname" --backtitle "$whiptail_title" --inputbox "Please specify a Short Hostname" --nocancel 10 50 3>&1 1>&2 2>&3)
+    done;
+  	
+  	#if $HOSTNAME_FULL is not defined do loop till user input
+    while [ $HOSTNAME_FULL == "" ]; do
+      HOSTNAME_FULL=$(whiptail --title "Fully Qualified Hostname" --backtitle "$whiptail_title" --inputbox "Please specify a Fully Qualified Hostname" --nocancel 10 50 3>&1 1>&2 2>&3)
+    done
+    
+    if (whiptail --title "Install Web Server" --backtitle "$whiptail_title" --yesno "Install Web Server?" 10 50) then
+    	install_web_server=true;
+    	while [ "$WEB_SERVER" == "" ]; do
+    	  WEB_SERVER=$(whiptail --title "Web Server" --backtitle "$whiptail_title" --nocancel --radiolist "Select Web Server Software" 10 50 2 "Nginx" "(default)" ON "Apache" "" OFF 3>&1 1>&2 2>&3)
+    	done;
+    else
+    	install_web_server=false;
+    fi;
+  fi;
+else
+  echo "Error: Your OS is not supported.";
+  exit 1;
+fi;
